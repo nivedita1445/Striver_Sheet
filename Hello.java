957bf4ba379f889c9ca647b476a3d78d14775a80
Nivedita Wani <niveditawani9@gmail.com>
2023-10-06 19:30:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@
 class Hello {
     public static void main(String niv[]){
         System.out.println("Hello Nivedita");
+        System.out.println("Successfully done the program");
     }
     
 }
